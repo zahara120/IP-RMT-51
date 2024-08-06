a267ba1721da9e30a6f26fe8ea923f059987bd6c
zahara120 <zaharaa120@gmail.com>
2024-08-06 15:42:55 +0700

    feat (server): add cors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const router = require("./routes");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
