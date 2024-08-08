a0eb796e1a6f13aa821ade4c4834acaf834aa419
zahara120 <zaharaa120@gmail.com>
2024-08-09 00:31:07 +0700

    feat (client): show star

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -47,12 +47,16 @@ export default function ReviewCard({ data }) {
   return (
     <>
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl">Reviews</h1>
+        {data?.Reviews?.length > 0 && (
+          <h1 className="text-2xl">{data?.Reviews?.length} Reviews</h1>
+        )}
         {isLogin && (
+          <>
           <Button className="bg-indigo-400 text-white" onPress={onOpen}>
             Add Review
             <FontAwesomeIcon icon="fa-regular fa-star" />
           </Button>
+          </>
         )}
       </div>
       <div className="flex flex-col gap-4 h-[500px] overflow-y-auto">
@@ -68,10 +72,21 @@ export default function ReviewCard({ data }) {
                 src: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
               }}
             />
-            <p className="flex flex-col text-sm">
-              <span>Rating: {el.rating}</span>
-              {el.comment}
-            </p>
+            <div className="flex items-center">
+              {Array.from({ length: el.rating }, (_, i) => (
+                <svg
+                  key={i}
+                  className="w-4 h-4 text-yellow-300 ms-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 22 20"
+                >
+                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+              ))}
+            </div>
+            <p>{el.comment}</p>
           </div>
         ))}
       </div>
@@ -105,11 +120,11 @@ export default function ReviewCard({ data }) {
                   Close
                 </Button>
                 <Button
-                  color="primary"
+                  className="bg-indigo-400 text-white"
                   isLoading={isLoading}
                   onClick={(e) => handleSubmit(e)}
                 >
-                  Action
+                  Submit
                 </Button>
               </ModalFooter>
             </>
