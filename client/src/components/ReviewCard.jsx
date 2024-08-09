045abed4e4f713b24501ad1019764bb47d1cf658
zahara120 <zaharaa120@gmail.com>
2024-08-09 23:23:52 +0700

    style (client): rating

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -68,20 +68,7 @@ export default function ReviewCard({ data }) {
                 src: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
               }}
             />
-            <div className="flex items-center">
-              {Array.from({ length: el.rating }, (_, i) => (
-                <svg
-                  key={i}
-                  className="w-4 h-4 text-yellow-300 ms-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 20"
-                >
-                  <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                </svg>
-              ))}
-            </div>
+            <Rating style={{ maxWidth: 120 }} value={el.rating} readOnly />
             <p>{el.comment}</p>
           </div>
         ))}
@@ -95,6 +82,7 @@ export default function ReviewCard({ data }) {
               </ModalHeader>
               <ModalBody>
                 <Rating
+                  isRequired
                   name="rating"
                   style={{ maxWidth: 180 }}
                   value={rating}
